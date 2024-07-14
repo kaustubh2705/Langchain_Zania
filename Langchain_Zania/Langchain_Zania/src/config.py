f2fec4990b0bec94e96047f02ebcb2794e8b2892
Kaustubh Khandelwal <32143524+kaustubh2705@users.noreply.github.com>
2024-07-14 21:39:24 +0530

    Delete Langchain_Zania directory

--- Langchain_Zania/Langchain_Zania/src/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    def __init__(self):
-        self.openai_api_key = os.getenv("OPENAI_API_KEY")
-        self.slack_token = os.getenv("SLACK_API_TOKEN")
-        self.slack_channel = os.getenv("SLACK_CHANNEL")
